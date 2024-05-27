754127602ce7922a1201753ff4dcdc2bcabfaa77
pccusir <wfx2@ulive.pccu.edu.tw>
2024-05-27 14:09:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    word=words_English_to_Chinese(msg)
+#    word=words_English_to_Chinese(msg)
     line_bot_api.reply_message(event.reply_token, TextSendMessage(word))
          
 
